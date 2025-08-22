5543112aab7965a7f6d1e055e7d78c3b1368fdd7
Federico A. Giglio <federicoagustingiglio@gmail.com>
2025-08-22 13:46:18 -0300

    Reorganización de carpeta

---
+++ Python/Clase1/Clase1_Ejercicio2.py
@@ -0,0 +1,5 @@
+#Ejercicio N°2: Crear un rango de numeros de 2 a 6 e imprimelos
+print("Rango de numero de 2 a 6")
+rango = range(2, 7)
+for i in rango:
+    print(i)
---
+++ Python/Clase1/Clase1_Ejercicio3.py
@@ -0,0 +1,4 @@
+#Ejercicio N°3: crear un rango de 3 a 10, pero con incremento de 2 en 2.
+print('Rango de 2 en 2')
+for i in range (3, 11, 2):
+    print(i)
