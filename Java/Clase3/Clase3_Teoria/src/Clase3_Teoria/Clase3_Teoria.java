c750e615c4c37f1b7926837253b6def036d8e27f
NicoGiglio <gigliosantiagonicolas@gmail.com>
2025-08-31 04:36:04 +0000

    Se agrega la teoría de la Clase3 de Java

---
+++ Java/Clase3/Clase3_Teoria/src/Clase3_Teoria/Clase3_Teoria.java
@@ -0,0 +1,20 @@
+//Clase3_Teoria: Clases y Objetos
+package Clase3_Teoria;
+
+/* OBJETOS: contienen ATRIBUTOS y MÉTODOS
+ - ATRIBUTOS: Características.
+ - MÉTODOS: Operaciones disponibles y posibles con el objeto.
+
+CLASE: conjunto de OBJETOS que comparten ATRIBUTOS y METODOS.
+
+INSTANCIAR es crear una Clase.
+
+*/
+
+//Llamamos a esta clase "Clase3_Teoria"
+public class Clase3_Teoria {
+    public static void main(String[] args) {
+        String nombre; //Estos son los ATRIBUTOS (características)
+        String apellido;        
+    }
+}
