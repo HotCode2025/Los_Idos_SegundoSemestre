aa07ebf626f52192ad008a4c4deec5fe8bf1abdb
Gabriel Soria <gabrieledgardosoria26@gmail.com>
2025-09-08 22:04:53 -0300

    Update Clase5_Ejercicio4.py

--- Python/Clase5/Clase5_Ejercicio4.py
+++ Python/Clase5/Clase5_Ejercicio4.py
@@ -1,3 +1,9 @@
+#Ejercicio2: Funcion con *args para multiplicar
+#crear una funcion para multiplicar los valores recibidos
+#de tipo numerico, utilizando argumentos variables *args
+#como parametro de la función y regresar como resultado
+#la multiplicación de todos los valores pasados como argumento
+
 def multiplicar(*args):
     """Multiplica todos los valores numéricos recibidos."""
     resultado = 1
